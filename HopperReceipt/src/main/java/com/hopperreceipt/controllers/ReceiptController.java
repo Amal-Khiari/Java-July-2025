3dca5bc07f7b30ba2d811abc7925e63e109c6d00
Amal-Khiari <khiariamal@yahoo.com>
2025-08-03 22:59:37 +0200

    Hopper's Receipt

---
+++ HopperReceipt/src/main/java/com/hopperreceipt/controllers/ReceiptController.java
@@ -0,0 +1,22 @@
+package com.hopperreceipt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReceiptController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Grace Hopper");
+        model.addAttribute("itemName", "Copper wire");
+        model.addAttribute("price", 5.25);
+        model.addAttribute("description", "Metal strips, also an illustration of nanoseconds.");
+        model.addAttribute("vendor", "Little Things Corner Store");
+
+        return "index.jsp";
+    }
+}
+
+
